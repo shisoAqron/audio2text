3dbbfa3d83f9cbaac1be87dbfe2b0e37e4030df4
shisoAqron <nooba704@gmail.com>
2025-03-05 14:36:57 +0900

    within skyway & fix starting with mediaStreamTrack

--- lib/SpeechRecognition.ts
+++ lib/SpeechRecognition.ts
@@ -10,6 +10,8 @@ export class SpeechRecognition {
   speech: globalThis.SpeechRecognition;
   onTranscript = new Event<onTranscriptObject>();
 
+  // MEMO: ここでspeechRecognitionを初期化するとglobalで呼び出した場合に複数streamに対応できない
+  // そもそも複数streamに対応できる？ -> できなさそう
   constructor() {
     const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
     if (!speechRecognition) {
@@ -36,7 +38,9 @@ export class SpeechRecognition {
     };
   }
 
+  // MEMO: 3/5時点でChromeで試してみたが今までの挙動をしているように見える -> M135から対応予定らしい．現在はCanaryやDevで確認できる
   // https://webaudio.github.io/web-speech-api/#dom-speechrecognition-start-audiotrack
+  // https://chromestatus.com/feature/5178378197139456
   streamStartWithTrack(track : MediaStreamTrack) {
     // @ts-ignore chromeのみ対応．typesが対応できてない
     this.speech.start(track);
@@ -50,8 +54,9 @@ export class SpeechRecognition {
         this.onTranscript.emit({ transcript: autotext });
       }
     }
-    this.speech.onend = () => { 
-      this.speech.start() 
+    this.speech.onend = () => {
+      // @ts-ignore chromeのみ対応．typesが対応できてない
+      this.speech.start(track) 
     };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,159 @@
+import {
+  type LocalStream,
+  type RemoteAudioStream,
+  type RoomPublication,
+  SkyWayAuthToken,
+  SkyWayContext,
+  SkyWayRoom,
+  SkyWayStreamFactory,
+  nowInSec,
+  uuidV4,
+} from '@skyway-sdk/room';
 import { SpeechRecognition } from '../lib'
 // import { SpeechRecognition } from '../dist';
 
-// deviceを事前に選択しておかないとここで現在有効なものが利用されるぽい？
-const speechRecognition = new SpeechRecognition();
+// -- SkyWay SDK --
+const appId = import.meta.env.VITE_SKYWAY_APPID;
+const secret = import.meta.env.VITE_SKYWAY_SECRET;
 
-const btn = document.getElementById('btn');
-const content = document.getElementById('content');
-const audioInputSelect = document.getElementById('audio-input-select') as HTMLSelectElement
-const audioInputSubmit = document.getElementById('audio-input-submit') as HTMLButtonElement
-let mediaStreamTrack = null as MediaStreamTrack | null
-
-if(!btn || !content || !audioInputSelect || !audioInputSelect) throw new Error('element not found');
-
-btn.addEventListener('click' , () => {
-  console.log('start');
-  // こちらはデフォルトのデバイスを使っているらしい
-  // speechRecognition.streamStart();
-
-  // startの引数が動くかの確認
-  if(mediaStreamTrack){
-    speechRecognition.streamStartWithTrack(mediaStreamTrack);
-  }
-});
-
-speechRecognition.onTranscript.addListener((obj) => {
-  console.log(obj);
-  // contentにelementを追加
-  const p = document.createElement('p');
-  p.textContent = obj.transcript;
-  content.appendChild(p);
-});
-
-const getDevices = async () => {
-  const devices = await navigator.mediaDevices.enumerateDevices().then((devices) => {
-    return devices.filter((device) => device.kind === 'audioinput')
-  })
-  return devices
+const createToken = () => {
+  const token = new SkyWayAuthToken({
+    jti: uuidV4(),
+    iat: nowInSec(),
+    exp: nowInSec() + 60 * 60 * 24,
+    version: 3,
+    scope: {
+      appId,
+      rooms: [{
+        id: '*',
+        methods: ['create', 'close', 'updateMetadata'],
+        member: {
+          id: '*',
+          methods: ['publish', 'subscribe', 'updateMetadata']
+        },
+        sfu: {
+          enabled: true,
+        }
+      }],
+      turn: {
+        enabled: true
+      },
+      analytics: {
+        enabled: true
+      },
+    },
+  }).encode(secret);
+  return token;
 }
 
-// 即時関数で初期化
+const buttonArea = document.getElementById('button-area')!;
+const remoteMediaArea = document.getElementById('remote-media-area')!;
+const roomNameInput = document.getElementById(
+  'room-name',
+) as HTMLInputElement;
+const myId = document.getElementById('my-id')!;
+const joinButton = document.getElementById('join')!;
+const leaveButton = document.getElementById('leave')!;
+
+// -- speech recognition --
+const textArea = document.getElementById('text-area') as HTMLDivElement;
+const audioInputSelect = document.getElementById('audio-input-select') as HTMLSelectElement
+
+// 即時関数で処理した方が良いものはこちらで実施
 (async () => {
-  const devices = await getDevices()
+  // デバイス一覧を取得してselectに追加
+  const devices = await SkyWayStreamFactory.enumerateInputAudioDevices()
   devices.forEach((device) => {
     const option = document.createElement('option')
-    option.id = device.deviceId
+    option.id = device.id
     option.textContent = device.label
     audioInputSelect.appendChild(option)
   })
-  audioInputSubmit.addEventListener('click', async () => {
-    const selectedId = audioInputSelect.selectedOptions[0].id
-    await navigator.mediaDevices.getUserMedia({
-      audio: { deviceId: selectedId }
-    }).then((stream) => {
-      console.log("set audio device", selectedId, stream);
-      const track = stream.getAudioTracks()[0]
-      mediaStreamTrack = track
+
+  // joinをした際にpublishや各種イベントハンドラの設定をする
+  joinButton.onclick = async () => {
+    if (roomNameInput.value === '') {
+      console.log('Please input room name');
+      return;
+    }
+  
+    // create context
+    const token = createToken();
+    const context = await SkyWayContext.Create(token);
+    context.onTokenUpdateReminder.add(() => {
+      context.updateAuthToken(createToken());
     });
-  })
+  
+    // room join
+    const room = await SkyWayRoom.FindOrCreate(context, {
+      type: 'p2p',
+      name: roomNameInput.value,
+    });
+    const me = await room.join();
+  
+    myId.textContent = me.id;
+
+
+    // get MediaStream & publish
+    const selectedId = audioInputSelect.selectedOptions[0].id
+    const mediaStream = await SkyWayStreamFactory.createMicrophoneAudioStream({
+      deviceId: selectedId
+    })
+    await me.publish(mediaStream);
+  
+    const subscribeAndAttach = (publication: RoomPublication<LocalStream>) => {
+      if (publication.publisher.id === me.id) {
+        return;
+      }
+  
+      const subscribeButton = document.createElement('button');
+      subscribeButton.id = `subscribe-button-${publication.id}`;
+      subscribeButton.textContent = `${publication.publisher.id}: ${publication.contentType}`;
+      buttonArea.appendChild(subscribeButton);
+  
+      subscribeButton.onclick = async () => {
+        const { stream } = await me.subscribe<RemoteAudioStream>(
+          publication.id,
+        );
+  
+        const newMedia = document.createElement('audio');
+        newMedia.controls = true;
+        newMedia.autoplay = true;
+        newMedia.id = `media-${publication.id}`;
+        stream.attach(newMedia);
+        remoteMediaArea.appendChild(newMedia);
+
+        // MEMO: M135からtrackが対応予定で，それ以前だと毎回呼ぶとクラッシュする(デバイスが認識されなくなる)
+        const speechRecognition = new SpeechRecognition();
+
+        speechRecognition.onTranscript.addListener((transObject) => {
+          console.log(transObject);
+          // contentにelementを追加
+          const p = document.createElement('p');
+          p.textContent = `${publication.publisher.id}:${transObject.transcript}`;
+          textArea.appendChild(p);
+        });
+
+        speechRecognition.streamStartWithTrack(stream.track);
+      };
+    };
+  
+    room.publications.forEach(subscribeAndAttach);
+    room.onStreamPublished.add((e) => subscribeAndAttach(e.publication));
+
+    leaveButton.onclick = async () => {
+      await me.leave();
+      await room.dispose();
+
+      myId.textContent = '';
+      buttonArea.replaceChildren();
+      remoteMediaArea.replaceChildren();
+    };
+
+    room.onStreamUnpublished.add((e) => {
+      document.getElementById(`subscribe-button-${e.publication.id}`)?.remove();
+      document.getElementById(`media-${e.publication.id}`)?.remove();
+    });
+  }
 })();
 
